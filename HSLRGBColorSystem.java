ffeda759de76b3f51110d762dc44538ad665453f
Klerisson Paixao <klerissonpaixao@gmail.com>
2016-03-17 17:51:48 +0100

    Update
    testing compare view update

--- HSLRGBColorSystem.java
+++ HSLRGBColorSystem.java
@@ -1,165 +1,209 @@
-/*
- * @(#)HSLRGBColorSystem.java
- *
- * Copyright (c) 2008 by the original authors of JHotDraw
- * and all its contributors.
- * All rights reserved.
- * 
- * The copyright of this software is owned by the authors and  
- * contributors of the JHotDraw project ("the copyright holders").  
- * You may not use, copy or modify this software, except in  
- * accordance with the license agreement you entered into with  
- * the copyright holders. For details see accompanying license terms. 
- */
-package org.jhotdraw.color;
+	/*
+	 * @(#)HSLColorSpace.java
+	 * 
+	 * Copyright (c) 2010 by the original authors of JHotDraw
+	 * and all its contributors.
+	 * All rights reserved.
+	 * 
+	 * The copyright of this software is owned by the authors and  
+	 * contributors of the JHotDraw project ("the copyright holders").  
+	 * You may not use, copy or modify this software, except in  
+	 * accordance with the license agreement you entered into with  
+	 * the copyright holders. For details see accompanying license terms. 
+	 */
+	package org.jhotdraw.color;
 
-import java.awt.*;
-import javax.swing.*;
+	import java.awt.color.ColorSpace;
 
-/**
- * A ColorSystem for HSL color components (hue, saturation, lightness) based
- * on the primary colors red, green and blue.
- *
- * @author  Werner Randelshofer
- * @version $Id: HSLRGBColorSystem.java 527 2009-06-07 14:28:19Z rawcoder $
- */
-public class HSLRGBColorSystem extends AbstractColorSystem {
+	/**
+	 * A HSL color space with additive complements in the hue color wheel:
+	 * red is opposite cyan, magenta is opposite green, blue is opposite yellow.
+	 *
+	 * @author Werner Randelshofer
+	 * @version $Id: HSLColorSpace.java 640 2010-01-22 16:43:45Z rawcoder $
+	 */
+	public class HSLColorSpace extends ColorSpace implements NamedColorSpace {
 
-    /**
-     * Creates a new instance.
-     */
-    public HSLRGBColorSystem() {
-    }
+	    private static HSLColorSpace instance;
 
-    public int toRGB(float... components) {
-        float hue = components[0];
-        float saturation = components[1];
-        float lightness = components[2];
+	    public static HSLColorSpace getInstance() {
+		if (instance == null) {
+		    instance = new HSLColorSpace();
+		}
+		return instance;
+	    }
 
-        // compute p and q from saturation and lightness
-        float q;
-        if (lightness < 0.5f) {
-            q = lightness * (1f + saturation);
-        } else {
-            q = lightness + saturation - (lightness * saturation);
-        }
-        float p = 2f * lightness - q;
+	    public HSLColorSpace() {
+		super(ColorSpace.TYPE_HSV, 3);
+	    }
 
-        // normalize hue to -1..+1
-        float hk = hue - (float) Math.floor(hue); // / 360f;
+	    @Override
+	    public float[] toRGB(float[] components) {
+		float hue = components[0];
+		float saturation = components[1];
+		float lightness = components[2];
 
-        // compute red, green and blue
-        float red = hk + 1f / 3f;
-        float green = hk;
-        float blue = hk - 1f / 3f;
+		// compute p and q from saturation and lightness
+		float q;
+		if (lightness < 0.5f) {
+		    q = lightness * (1f + saturation);
+		} else {
+		    q = lightness + saturation - (lightness * saturation);
+		}
+		float p = 2f * lightness - q;
 
-        // normalize rgb values
-        if (red < 0) {
-            red = red + 1f;
-        } else if (red > 1) {
-            red = red - 1f;
-        }
+		// normalize hue to -1..+1
+		float hk = hue - (float) Math.floor(hue); // / 360f;
 
-        if (green < 0) {
-            green = green + 1f;
-        } else if (green > 1) {
-            green = green - 1f;
-        }
+		// compute red, green and blue
+		float red = hk + 1f / 3f;
+		float green = hk;
+		float blue = hk - 1f / 3f;
 
-        if (blue < 0) {
-            blue = blue + 1f;
-        } else if (blue > 1) {
-            blue = blue - 1f;
-        }
+		// normalize rgb values
+		if (red < 0) {
+		    red = red + 1f;
+		} else if (red > 1) {
+		    red = red - 1f;
+		}
 
+		if (green < 0) {
+		    green = green + 1f;
+		} else if (green > 1) {
+		    green = green - 1f;
+		}
 
-        // adjust rgb values
-        if (red < 1f / 6f) {
-            red = p + ((q - p) * 6 * red);
-        } else if (red < 0.5f) {
-            red = q;
-        } else if (red < 2f / 3f) {
-            red = p + ((q - p) * 6 * (2f / 3f - red));
-        } else {
-            red = p;
-        }
-
-        if (green < 1f / 6f) {
-            green = p + ((q - p) * 6 * green);
-        } else if (green < 0.5f) {
-            green = q;
-        } else if (green < 2f / 3f) {
-            green = p + ((q - p) * 6 * (2f / 3f - green));
-        } else {
-            green = p;
-        }
-
-        if (blue < 1f / 6f) {
-            blue = p + ((q - p) * 6 * blue);
-        } else if (blue < 0.5f) {
-            blue = q;
-        } else if (blue < 2f / 3f) {
-            blue = p + ((q - p) * 6 * (2f / 3f - blue));
-        } else {
-            blue = p;
-        }
+		if (blue < 0) {
+		    blue = blue + 1f;
+		} else if (blue > 1) {
+		    blue = blue - 1f;
+		}
 
 
-        // pack red, green and blue into 24-bit rgb
-        int rgb = ((int) (red * 255)) << 16 |
-                ((int) (green * 255)) << 8 |
-                ((int) (blue * 255));
+		// adjust rgb values
+		if (red < 1f / 6f) {
+		    red = p + ((q - p) * 6 * red);
+		} else if (red < 0.5f) {
+		    red = q;
+		} else if (red < 2f / 3f) {
+		    red = p + ((q - p) * 6 * (2f / 3f - red));
+		} else {
+		    red = p;
+		}
 
-        return rgb;
-    }
+		if (green < 1f / 6f) {
+		    green = p + ((q - p) * 6 * green);
+		} else if (green < 0.5f) {
+		    green = q;
+		} else if (green < 2f / 3f) {
+		    green = p + ((q - p) * 6 * (2f / 3f - green));
+		} else {
+		    green = p;
+		}
 
-    public float[] toComponents(int red, int green, int blue, float[] components) {
-        if (components == null || components.length != 3) {
-            components = new float[3];
-        }
+		if (blue < 1f / 6f) {
+		    blue = p + ((q - p) * 6 * blue);
+		} else if (blue < 0.5f) {
+		    blue = q;
+		} else if (blue < 2f / 3f) {
+		    blue = p + ((q - p) * 6 * (2f / 3f - blue));
+		} else {
+		    blue = p;
+		}
 
-        float r = red / 255f;
-        float g = green / 255f;
-        float b = blue / 255f;
 
-        float max = Math.max(Math.max(r, g), b);
-        float min = Math.min(Math.min(r, g), b);
+		return new float[]{red, green, blue};
+	    }
 
-        float hue;
-        float saturation;
-        float luminance;
+	    @Override
+	    public float[] fromRGB(float[] rgbvalue) {
+		float r = rgbvalue[0];
+		float g = rgbvalue[1];
+		float b = rgbvalue[2];
 
-        if (max == min) {
-            hue = 0;
-        } else if (max == r && g >= b) {
-            hue = 60f * (g - b) / (max - min);
-        } else if (max == r && g < b) {
-            hue = 60f * (g - b) / (max - min) + 360f;
-        } else if (max == g) {
-            hue = 60f * (b - r) / (max - min) + 120f;
-        } else /*if (max == b)*/ {
-            hue = 60f * (r - g) / (max - min) + 240f;
-        }
+		float max = Math.max(Math.max(r, g), b);
+		float min = Math.min(Math.min(r, g), b);
 
-        luminance = (max + min) / 2f;
+		float hue;
+		float saturation;
+		float luminance;
 
-        if (max == min) {
-            saturation = 0;
-        } else if (luminance <= 0.5f) {
-            saturation = (max - min) / (max + min);
-        } else /* if (lightness  > 0.5f)*/ {
-            saturation = (max - min) / (2 - (max + min));
-        }
+		if (max == min) {
+		    hue = 0;
+		} else if (max == r && g >= b) {
+		    hue = 60f * (g - b) / (max - min);
+		} else if (max == r && g < b) {
+		    hue = 60f * (g - b) / (max - min) + 360f;
+		} else if (max == g) {
+		    hue = 60f * (b - r) / (max - min) + 120f;
+		} else /*if (max == b)*/ {
+		    hue = 60f * (r - g) / (max - min) + 240f;
+		}
 
-        components[0] = hue / 360f;
-        components[1] = saturation;
-        components[2] = luminance;
+		luminance = (max + min) / 2f;
 
-        return components;
-    }
+		if (max == min) {
+		    saturation = 0;
+		} else if (luminance <= 0.5f) {
+		    saturation = (max - min) / (max + min);
+		} else /* if (lightness  > 0.5f)*/ {
+		    saturation = (max - min) / (2 - (max + min));
+		}
 
-    public int getComponentCount() {
-        return 3;
-    }
+		return new float[]{
+		            hue / 360f,
+		            saturation,
+		            luminance};
+	    }
 
-}
+	    @Override
+	    public float[] toCIEXYZ(float[] colorvalue) {
+		float[] rgb = toRGB(colorvalue);
+		return ColorSpace.getInstance(CS_sRGB).toCIEXYZ(rgb);
+	    }
+
+	    @Override
+	    public float[] fromCIEXYZ(float[] colorvalue) {
+		float[] sRGB = ColorSpace.getInstance(ColorSpace.CS_sRGB).fromCIEXYZ(colorvalue);
+		return fromRGB(sRGB);
+	    }
+
+	    @Override
+	    public String getName(int idx) {
+		switch (idx) {
+		    case 0:
+		        return "Hue";
+		    case 1:
+		        return "Saturation";
+		    case 2:
+		        return "Lightness";
+		    default:
+		        throw new IllegalArgumentException("index must be between 0 and 2:" + idx);
+		}
+	    }
+
+	    @Override
+	    public float getMaxValue(int component) {
+		return 1f;
+	    }
+
+	    @Override
+	    public float getMinValue(int component) {
+		return 0f;
+	    }
+
+	    @Override
+	    public boolean equals(Object o) {
+		return (o instanceof HSLColorSpace);
+	    }
+
+	    @Override
+	    public int hashCode() {
+
+		return getClass().getSimpleName().hashCode();
+	    }
+	    @Override
+	    public String getName() {
+		return "HSL";
+	    }
+	}
